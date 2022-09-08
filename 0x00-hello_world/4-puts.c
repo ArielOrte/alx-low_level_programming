9928ec1212e17cf15a16d990c510d981c19dc84a
Ariel <arielchrisortega@gmail.com>
2022-09-08 07:20:18 -0700

    Updates on learning C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,20 +3,16 @@
 
 
 /**
+ * main - Entry point
+ * Return: Always 0 (Success)
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ */
 
-int main(void)
+int main(void) 
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle"); /* Print the string of characters */
 
 			return (0);
 
