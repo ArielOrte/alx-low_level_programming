dba2bf05199bfc935645ff46d026e2d7b5d36060
Ariel <arielchrisortega@gmail.com>
2022-09-09 15:00:29 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ * Return: Always o
+ */
+
+
+
+/* betty style doc for function main goes there */
+
+int main(void)
+
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+	putchar(alphabets);
+	}
+
+	putchar('\n');
+
+					return (0);
+
+}
