e863af1d66f3d2828d52ab46443e9aad7896ef92
Ariel <arielchrisortega@gmail.com>
2022-09-13 23:03:59 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch[8] = "_putchar";
+
+	int i = 0;
+		while (i <= 8)
+		{
+
+			_putchar(ch[i]);
+			i++;
+		}
+	_putchar('\n');
+	return (0);
+}
