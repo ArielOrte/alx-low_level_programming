b6c9076224fb877cb3e191702ab487ffbc2f7448
Ariel <arielchrisortega@gmail.com>
2022-09-30 04:32:04 -0700

    program that prints all arguments it receives

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Prints the number of arguments passed to it
+ * @argc: counts arguments used
+ * @argv: arguments used
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
