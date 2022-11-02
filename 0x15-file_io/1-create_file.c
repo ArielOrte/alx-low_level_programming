9fc404d7e135244a7c6e88dc3367df462882b781
Ariel <arielchrisortega@gmail.com>
2022-11-02 05:25:52 -0700

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,6 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
@@ -19,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 
 	}
 
-	fd = open(filename, 0_CREAT | 0_RDWR | 0_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(fd, text_content, len);
 
 	if (fd == -1 || wr == -1)
